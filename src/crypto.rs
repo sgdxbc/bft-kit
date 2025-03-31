001d9f6b88d91a9fc25c603e0d1a3830af49eab8
sgdxbc <sung@comp.nus.edu.sg>
2025-03-31 22:13:05 +0800

    refactor(crypto): switch Sig to newtype

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,4 +1,4 @@
-use std::fmt::Debug;
+use std::fmt::{Debug, Display};
 
 use bincode::{Decode, Encode};
 
@@ -6,16 +6,36 @@ use crate::common::fmt_bytes;
 
 pub mod cert;
 
-// wire type for signature
-// erasing type for simple (de)serialization
-pub type Sig = Vec<u8>;
-
 #[derive(Clone, PartialEq, Eq, Encode, Decode)]
 pub struct Digest(pub Vec<u8>);
 
-impl Debug for Digest {
+impl Display for Digest {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Digest")?;
         fmt_bytes(&self.0, f)
+    }
+}
+
+impl Debug for Digest {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self}")
+    }
+}
+
+// wire type for signature
+// erasing type for simple (de)serialization
+#[derive(Clone, Default, Encode, Decode)]
+pub struct Sig(pub Vec<u8>);
+
+impl Display for Sig {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Sig")?;
+        fmt_bytes(&self.0, f)
+    }
+}
+
+impl Debug for Sig {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self}")
     }
 }
