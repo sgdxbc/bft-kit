99f1815d7519e2fe998787fd99c2db29e47a4b23
sgdxbc <sung@comp.nus.edu.sg>
2025-04-12 14:30:30 +0800

    refactor(common): extract report latencies into workload

--- src/bin/pbft-client.rs
+++ src/bin/pbft-client.rs
@@ -1,12 +1,11 @@
-use std::{env::args, path::PathBuf, time::Duration};
+use std::{env::args, path::PathBuf};
 
 use bft_testbed::{
-    common::parse::Options,
+    common::{parse::Options, workload::report_latencies},
     init_logging,
     pbft::transport::{TaskConfig, WARMUP_DURATION, run_close_loop_clients, tcp},
 };
 use futures::FutureExt;
-use hdrhistogram::Histogram;
 use tokio::fs::read_to_string;
 use tracing::Instrument;
 
@@ -31,48 +30,7 @@ async fn main() -> anyhow::Result<()> {
     }
     .instrument(tracing::info_span!("concurrent close loops"))
     .await?;
-    let num_client_latencies = client_latencies.len();
-    let mut latencies = Histogram::new(3)?;
-    for client_latencies in client_latencies {
-        let throughput = client_latencies.len() as f32
-            / (config.client_duration - WARMUP_DURATION).as_secs_f32();
-        let latency_mean = client_latencies.mean() / 1_000_000.;
-        tracing::info!(
-            "client throughput {throughput:.2} ({:.2} = inverse of mean latency {:?})",
-            1. / latency_mean,
-            Duration::from_secs_f64(latency_mean),
-        );
-        latencies += client_latencies
-    }
-    let throughput =
-        latencies.len() as f32 / (config.client_duration - WARMUP_DURATION).as_secs_f32();
-    let latency_mean = latencies.mean() / 1_000_000.;
-    tracing::info!(
-        "throughput {throughput:.2} ({:.2} = {num_client_latencies} * inverse of mean latency {:?})",
-        num_client_latencies as f64 / latency_mean,
-        Duration::from_secs_f64(latency_mean),
-    );
-    for value in latencies.iter_quantiles(1) {
-        if value.count_since_last_iteration() == 0 {
-            continue;
-        }
-        tracing::info!(
-            "quantile {:8.6} latency {:10?}",
-            value.quantile(),
-            Duration::from_micros(value.value_iterated_to()),
-        )
-    }
-    for value in latencies.iter_log(1, 2.) {
-        let count = value.count_since_last_iteration();
-        if count == 0 {
-            continue;
-        }
-        tracing::info!(
-            "{:8} ({:4.2}) requests <= {:?}",
-            count,
-            count as f32 / latencies.len() as f32,
-            Duration::from_micros(value.value_iterated_to())
-        );
-    }
+
+    report_latencies(client_latencies, config.client_duration - WARMUP_DURATION);
     Ok(())
 }
--- src/common/workload.rs
+++ src/common/workload.rs
@@ -1,5 +1,6 @@
 use std::{collections::HashMap, time::Duration};
 
+use hdrhistogram::Histogram;
 use rand::random;
 use tokio::{
     sync::mpsc::{self, Receiver, Sender},
@@ -86,5 +87,49 @@ impl ConcurrentClients {
 impl Default for ConcurrentClients {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+pub fn report_latencies(client_latencies: Vec<Latencies>, duration: Duration) {
+    let num_client_latencies = client_latencies.len();
+    let mut latencies = Histogram::new(3).expect("valid histogram parameter");
+    for client_latencies in client_latencies {
+        let throughput = client_latencies.len() as f32 / duration.as_secs_f32();
+        let latency_mean = client_latencies.mean() / 1_000_000.;
+        tracing::info!(
+            "client throughput {throughput:.2} ({:.2} = inverse of mean latency {:?})",
+            1. / latency_mean,
+            Duration::from_secs_f64(latency_mean),
+        );
+        latencies += client_latencies
+    }
+    let throughput = latencies.len() as f32 / duration.as_secs_f32();
+    let latency_mean = latencies.mean() / 1_000_000.;
+    tracing::info!(
+        "throughput {throughput:.2} ({:.2} = {num_client_latencies} * inverse of mean latency {:?})",
+        num_client_latencies as f64 / latency_mean,
+        Duration::from_secs_f64(latency_mean),
+    );
+    for value in latencies.iter_quantiles(1) {
+        if value.count_since_last_iteration() == 0 {
+            continue;
+        }
+        tracing::info!(
+            "quantile {:8.6} latency {:10?}",
+            value.quantile(),
+            Duration::from_micros(value.value_iterated_to()),
+        )
+    }
+    for value in latencies.iter_log(1, 2.) {
+        let count = value.count_since_last_iteration();
+        if count == 0 {
+            continue;
+        }
+        tracing::info!(
+            "{:8} ({:4.2}) requests <= {:?}",
+            count,
+            count as f32 / latencies.len() as f32,
+            Duration::from_micros(value.value_iterated_to())
+        );
     }
 }
